b6be5344322c4278af957e77a84a702ad8abf450
Luís-Felipe <luis.felipeoliveirafrancisco@gmail.com>
2023-05-05 00:37:15 -0300

    commit foda
    foda commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <title>Fatraca</title>  
+    </head>
+    <body>
+        <header class="header">
+            <div class="container">
+                <div class="titulo">
+                    <a href="#">Fatraca</a>
+                </div><!-- titulo -->
+
+                <nav class="menu-desktop">
+                    <ul>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Cadastro</a></li>
+                    </ul>
+                </nav><!-- menu-desktop -->
+                <div class="clear"></div>
+            </div><!-- container -->
+        </header>
+
+        <section class="conteudo">
+
+        </section><!-- conteudo -->
+    </body>
+</html>
