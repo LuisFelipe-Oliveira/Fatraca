bf16d998208e5aab63ea28723c98922050955f5f
HenriBarboza <henr.fe7@gmail.com>
2023-05-09 11:04:13 -0300

    Pagina de login
    Criação e estilização da pagina de login com imagens

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets\css\styleLogin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Login</title>
+</head>
+<body>
+    <div class="main-login">
+        <div class="left-login">
+            <h1>Faça login e conheça a Fatec</h1>
+                <div id="left-login-image">
+                    <img src="assets\imgs\loginImg.svg" alt="">
+                </div>
+        </div>
+
+        <div class="right-login">
+            <div class="card-login">
+                <h1>Login</h1>
+                <div class="textfield">
+                    <label for="usuario">Usuário</label>
+                    <input type="text" name="usuario" placeholder="Usuário">    
+                </div>
+                <div class="textfield">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="Senha">    
+                </div>
+                <button class="btn-login">Login</button>
+                <butoton class="entrar-com">
+                    <span class="entrar-txt">Entrar com</span>
+                    <div class="e-btn">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-google"></i></a>
+                        <a href="#"><i class="fab fa-microsoft"></i></a>
+                    </div>
+                </butoton>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
