e60df2312f69a611495830a7a2089406ff43d5ac
Luís-Felipe <luis.felipeoliveirafrancisco@gmail.com>
2023-05-11 15:14:59 -0300

    login funcionando
    funciona

--- login.php
+++ login.php
@@ -1,3 +1,66 @@
+<?php
+
+$flag_msg = null;
+
+$msg = "";
+
+if(isset($_POST["enviar"])) {
+    require("./config/connect.php");
+
+    $usuario = $_POST["usuario"];
+    $senha = $_POST["senha"];
+
+    $form = [$usuario, $senha];
+
+    $aux = 0;
+
+    foreach ($form as $field) {
+        if (empty($field)) {
+            $aux += 1;
+        }
+    }
+
+    if ($aux > 0) {
+        $flag_msg = false;
+        $msg = "preencha todos os campos!";
+    } else {
+
+        $senha_encrypt = md5($senha);
+
+        try {
+
+            $sql = "SELECT * FROM fatraca_users WHERE usuario = :usuario AND senha = :senha";
+
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindValue(':usuario', $usuario);
+            $stmt->bindValue(':senha', $senha_encrypt);
+
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                $msg = "Sucesso!";
+            } else {
+                $msg = "Erro!";
+            }
+
+            $flag_msg = true; // Sucesso 
+
+        } catch (PDOException $th) {
+            $conn = null;
+
+            $flag_msg = false; //Erro 
+            $msg = "Erro na conexão com o Banco de dados: " . $th->getMessage();
+        }
+
+    }
+
+    $conn = null;
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -13,6 +76,17 @@
 </head>
 
 <body>
+
+    <?php
+    if (!is_null($flag_msg)) {
+        if ($flag_msg) {
+            echo "<div class='alert alert-success' role='alert'>$msg</div>";
+        } else {
+            echo "<div class='alert alert-warning' role='alert'>$msg</div>";
+        }
+    }
+    ?>
+
     <div class="main-login">
         <a href="index.php" id="logo">Fatraca</a>
         <div class="left-login">
@@ -21,34 +95,35 @@
                 <img src="assets\imgs\loginImg.svg" alt="">
             </div>
         </div>
+        <form method="post">
+            <div class="right-login">
+                <div class="card-login">
+                    <h1>Login</h1>
 
-        <div class="right-login">
-            <div class="card-login">
-                <h1>Login</h1>
-
-                <div class="textfield">
-                    <label for="usuario">Usuário</label>
-                    <input type="text" name="usuario" placeholder="Usuário">
-                </div>
-                <div class="textfield">
-                    <label for="senha">Senha</label>
-                    <input type="password" name="senha" placeholder="Senha">
-                </div>
-                <a href="" class="btn-alt">Esqueceu a senha?</a>
-                <a href="cadastro.php" class="btn-alt">Criar conta</a>
-                <button class="btn-login">Login</button>
-                <butoton class="entrar-com">
-                    <span class="entrar-txt">Entrar com</span>
-                    <div class="e-btn">
-                        <a href="#"><i class="fab fa-facebook"></i></a>
-                        <a href="#"><i class="fab fa-google"></i></a>
-                        <a href="#"><i class="fab fa-microsoft"></i></a>
+                    <div class="textfield">
+                        <label for="usuario">Usuário</label>
+                        <input type="text" name="usuario" placeholder="Usuário">
                     </div>
-                </butoton>
+                    <div class="textfield">
+                        <label for="senha">Senha</label>
+                        <input type="password" name="senha" placeholder="Senha">
+                    </div>
+                    <a href="" class="btn-alt">Esqueceu a senha?</a>
+                    <a href="cadastro.php" class="btn-alt">Criar conta</a>
+                    <button class="btn-login" name="enviar">Login</button>
+                    <butoton class="entrar-com">
+                        <span class="entrar-txt">Entrar com</span>
+                        <div class="e-btn">
+                            <a href="#"><i class="fab fa-facebook"></i></a>
+                            <a href="#"><i class="fab fa-google"></i></a>
+                            <a href="#"><i class="fab fa-microsoft"></i></a>
+                        </div>
+                    </butoton>
+
+                </div>
 
             </div>
-        </div>
-
+        </form>
     </div>
 </body>
 
